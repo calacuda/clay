bbc28c1a5e5a60fffd3ce51b5a29d7725a9edf30
calacuda <eowest@gmail.com>
2021-07-19 09:40:04 -0400

    flushed out argument passing for dynamicly load compiled rust libs.

--- src/std_lib.rs
+++ src/std_lib.rs
@@ -4,19 +4,16 @@ TODO's:
 will print with out the space in between the tokens.
 */
 
-use crate::parser;
+use crate::bcc;
+use crate::bcc::{Bytecode, Nargs};
 use crate::lexer;
 use crate::lexer::Token;
-use crate::bcc;
-use crate::bcc::{
-    Bytecode,
-    Nargs
-};
+use crate::parser;
 
-use std::collections::HashSet;
 use std::collections::HashMap;
-use std::path::Path;
+use std::collections::HashSet;
 use std::env;
+use std::path::Path;
 
 use libloading;
 
@@ -43,26 +40,27 @@ pub fn from<'a>(imports: &Vec<Token>) -> Result<Option<Token>, &'a str> {
     FUNC return val must be in the form of a string like this "42=str", or "42=num". this
     will be translated into a Str Token or a number token respectively.
      */
-    let site_packs = format!("{}/.local/lib/clay/site-packages/", env::var("HOME").unwrap());
+    let site_packs = format!(
+        "{}/.local/lib/clay/site-packages/",
+        env::var("HOME").unwrap()
+    );
     let cur_dir = env::current_dir().unwrap();
     // println!("{:?}", &imports[1]);
     let mut lib_name = match &imports[1] {
-	Token::Symbol(l_name) => l_name.to_string(),
-	Token::Str(l_name) => l_name.to_string(),
-	_ => panic!("I lib name must be a symbol or string, can't be other data literal"),
+        Token::Symbol(l_name) => l_name.to_string(),
+        Token::Str(l_name) => l_name.to_string(),
+        _ => panic!("I lib name must be a symbol or string, can't be other data literal"),
     };
 
     if lib_name.contains('~') {
         lib_name = lib_name.replace("~", env::var("HOME").unwrap().as_str().as_ref());
     }
 
-    let pack_path = Path::new(&site_packs)
-                        .join(Path::new(&lib_name));
-                        // .as_os_str();
+    let pack_path = Path::new(&site_packs).join(Path::new(&lib_name));
+    // .as_os_str();
 
-    let cur_path = Path::new(cur_dir.as_os_str())
-                        .join(Path::new(&lib_name));
-                        //.as_os_str();
+    let cur_path = Path::new(cur_dir.as_os_str()).join(Path::new(&lib_name));
+    //.as_os_str();
 
     let full_path = Path::new(&lib_name);
 
@@ -72,39 +70,41 @@ pub fn from<'a>(imports: &Vec<Token>) -> Result<Option<Token>, &'a str> {
     };
 
     let func_name = match &data[0] {
-	Token::Symbol(lib_name) => lib_name,
-	Token::Str(lib_name) => lib_name,
-	_ => panic!("I func name must be a symbol or string, can't be other data literal"),
+        Token::Symbol(lib_name) => lib_name,
+        Token::Str(lib_name) => lib_name,
+        _ => panic!("I func name must be a symbol or string, can't be other data literal"),
     };
 
     // println!("cur_path :  {:?}", &cur_path);
     // println!("full_path :  {:?}", &full_path);
     // println!("pack_path :  {:?}", &pack_path);
 
-    let lib_path =
-	if cur_path.exists() {
+    let lib_path = if cur_path.exists() {
         // println!("cur_path");
-	    cur_path.as_os_str()
+        cur_path.as_os_str()
     } else if full_path.exists() {
         // println!("full_path");
         full_path.as_os_str()
-	} else if pack_path.exists() {
+    } else if pack_path.exists() {
         // println!("pack_path");
-	    pack_path.as_os_str()
-	} else {
-	    panic!("the requested module is not available");
-	};
+        pack_path.as_os_str()
+    } else {
+        panic!("the requested module is not available");
+    };
+
+    let args = make_f_args(data[1..].to_vec());
 
     unsafe {
         // let lib = libloading::Library::new(libloading::library_filename(lib_path)).unwrap();
         let lib = libloading::Library::new(lib_path).unwrap();
-        let func: libloading::Symbol<unsafe extern fn(Vec<Token>) ->
-                                     Result<Option<(String, String)>, &'a str>> =
-                                     lib.get(func_name.as_bytes())
-	                                    .unwrap();
+        let func: libloading::Symbol<
+            unsafe extern "C" fn(
+                Vec<(String, String)>,
+            ) -> Result<Option<(String, String)>, &'a str>,
+        > = lib.get(func_name.as_bytes()).unwrap();
         let tok: Token;
-        let raw_out =
-        match func(data[1..].to_vec()) {
+        // println!("{:?}", args);
+        let raw_out = match func(args) {
             Ok(Some(val)) => val,
             Ok(None) => return Ok(None),
             Err(error) => panic!("{:?}", error),
@@ -121,21 +121,34 @@ pub fn from<'a>(imports: &Vec<Token>) -> Result<Option<Token>, &'a str> {
     }
 }
 
-// fn make_tok<'input>(output: &str, ty: &'input str) -> Token<'input> {
-//     match ty.to_lowercase().as_ref() {
-// 	    "str" => Token::Str(output.to_string().as_str()),
-// 	    "bool" => Token::Bool(make_bool(&output)),
-// 	    "symbol" => Token::Symbol(output.to_owned().as_str()),
-// 	    "num" => Token::Number(output.to_owned()),
-// 	    _ => panic!("you cant return the datatype, {}", ty)
-// 	}
-// }
+fn make_f_args(clay_in: Vec<Token>) -> Vec<(String, String)> {
+    let mut rust_args: Vec<(String, String)> = Vec::new();
+    for arg in clay_in.iter() {
+        match arg {
+            Token::Str(data) => rust_args.push((data.to_owned(), "str".to_string())),
+            Token::Number(data) => rust_args.push((data.to_owned(), "num".to_string())),
+            Token::Bool(data) => {
+                if data.to_owned() {
+                    rust_args.push(("t".to_string(), "bool".to_string()))
+                } else {
+                    rust_args.push(("nil".to_string(), "bool".to_string()))
+                }
+            }
+            Token::Form(_) => panic!("you can't pass Forms as arguments to external libs yet"),
+            Token::Symbol(_) => {
+                panic!("you can't pass Symbols as arguments to external libs yet")
+            }
+            _ => panic!("syntax error, make sure your arguments are formated corectly."),
+        }
+    }
+    return rust_args;
+}
 
 fn make_bool(input: &str) -> bool {
     match input {
-	"t" => true,
-	"nill" => false,
-	_ => panic!("external library function returned a not boolean but clamed it as a bool."),
+        "t" => true,
+        "nill" => false,
+        _ => panic!("external library function returned a not boolean but clamed it as a bool."),
     }
 }
 
@@ -151,13 +164,11 @@ pub fn not<'a>(truth: &Vec<Token>) -> Result<Option<Token>, &'a str> {
     return Ok(Some(Token::Bool(negation)));
 }
 
-
 pub fn write_line<'a>(lines: &Vec<Token>) -> Result<Option<Token>, &'a str> {
     let _ = write(lines);
     println!();
     return Ok(None);
 }
-
 
 pub fn write<'a>(lines: &Vec<Token>) -> Result<Option<Token>, &'a str> {
     // println!("write args: {:?}", lines);
@@ -166,7 +177,7 @@ pub fn write<'a>(lines: &Vec<Token>) -> Result<Option<Token>, &'a str> {
     for i in 0..last_i {
         let line = loc_lines.pop().unwrap();
         print_line(line);
-        if i != last_i-1 {
+        if i != last_i - 1 {
             print!(" ");
         }
     }
@@ -179,7 +190,13 @@ fn print_line(line: Token) {
         Token::Number(output) => print!("{}", output),
         Token::Str(output) => print!("{}", output),
         Token::Form(form) => print_form(*form),
-        Token::Bool(truth) => print!("{}", match truth {true => "t", false => "nil"}),
+        Token::Bool(truth) => print!(
+            "{}",
+            match truth {
+                true => "t",
+                false => "nil",
+            }
+        ),
         _ => {} //return Err("ERROR: on write_line. you can't print that."),
     }
 }
@@ -197,9 +214,20 @@ pub fn terpri<'a>(_things: &Vec<Token>) -> Result<Option<Token>, &'a str> {
     return Ok(None);
 }
 
-
-pub fn get_std_funcs<'a>() -> HashMap<&'a str, (Nargs, &'a (dyn Fn(&Vec<Token>) -> Result<Option<Token>, &'a str>))> {
-    let mut std_funcs: HashMap<&'a str, (Nargs, &'a (dyn Fn(&Vec<Token>) -> Result<Option<Token>, &'a str>))> = HashMap::new();
+pub fn get_std_funcs<'a>() -> HashMap<
+    &'a str,
+    (
+        Nargs,
+        &'a (dyn Fn(&Vec<Token>) -> Result<Option<Token>, &'a str>),
+    ),
+> {
+    let mut std_funcs: HashMap<
+        &'a str,
+        (
+            Nargs,
+            &'a (dyn Fn(&Vec<Token>) -> Result<Option<Token>, &'a str>),
+        ),
+    > = HashMap::new();
 
     std_funcs.insert("write", (Nargs::INF, &write));
     std_funcs.insert("write-line", (Nargs::INF, &write_line));
