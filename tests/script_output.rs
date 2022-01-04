ddf55e65f481f0b9262920a542e8b04f032f890a
yogurt <eowest@gmail.com>
2022-01-04 02:18:06 -0500

    fixed tests

--- tests/script_output.rs
+++ tests/script_output.rs
@@ -6,7 +6,7 @@ fn test_file(name: &str) {
     let dif = Command::new("bash")
         .arg("-c")
 	//.arg("pwd")                                                                                                                               
-        .arg(format!("./target/debug/clay jit test_inputs/{fname}.lisp | diff test_output/{fname}.out -", fname=&name))
+        .arg(format!("./target/debug/clay test_inputs/{fname}.lisp | diff test_output/{fname}.out -", fname=&name))
         //.arg("|")
 	//.arg(format!("diff test_ouput/{fname}.out -\"", fname=&name))
         .output()
@@ -20,18 +20,22 @@ fn test_file(name: &str) {
 #[test]
 fn scripts() {
     use std::process::Command;
-    
+
+    let mut i = 1;
     let paths = fs::read_dir("./test_inputs").unwrap();
+    let num_files = fs::read_dir("./test_inputs").unwrap().count();
+
     Command::new("cargo build");
     for fname in paths {
         let name = match fname.unwrap().file_name().into_string() {
             Ok(f) => {
-                println!("\nrunning test on file :  {:?}", f);
+                println!("\nrunning test on file :  {:?}  :  [{}/{}]", f, i, num_files);
                 f.split(".").collect::<Vec<&str>>()[0].to_owned()
             },
             Err(_) => panic!("an error ocured in the testing script."),
         };                                                      
         test_file(&name);
+        i += 1;
     }
 }
 
